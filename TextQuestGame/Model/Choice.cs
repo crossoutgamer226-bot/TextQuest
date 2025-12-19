48de40d1e682c82f0965ecd57079ad9fdb64bdf3
gomselars@gmail.com <gomselars@gmail.com>
2025-12-19 17:38:07 +0700

    Добавила классы Choice, GameService, GameState, Scene. Обновила Program

---
+++ TextQuestGame/Model/Choice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextQuestGame
+{
+    [Serializable]
+    public class Choice
+    {
+        public string Text { get; set; }
+        public string NextSceneId { get; set; }
+    }
+
+}
--- TextQuestGame/Program.cs
+++ TextQuestGame/Program.cs
@@ -1,17 +1,18 @@
 namespace TextQuestGame
 {
-    internal static class Program
+    static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            var form = new MainForm();
+            var game = new GameService();
+            var presenter = new GamePresenter(form, game);
+
+            Application.Run(form);
         }
     }
 }
