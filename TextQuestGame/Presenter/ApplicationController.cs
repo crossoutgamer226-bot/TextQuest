7b9cae606fc8d14cd4b209c85aa74a2e20403cae
Каракулло Лев <CrossoutGamer226@gmail.com>
2025-12-19 18:32:53 +0600

    Добавил метод инициализации

--- TextQuestGame/Presenter/ApplicationController.cs
+++ TextQuestGame/Presenter/ApplicationController.cs
@@ -9,5 +9,30 @@ namespace TextQuestGame.Presenter
         private GamePresenter _presenter;
         private IGameService _gameService;
         private IGameView _view;
-    }
+        public void Start()
+        {
+            try
+            {
+           
+                CheckRequiredFiles();
+
+                
+                _gameService = new GameService();
+
+                
+                _view = new MainForm();
+
+                
+                _presenter = new GamePresenter(_view, _gameService);
+
+                
+                Application.Run(_view as Form);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Критическая ошибка запуска: {ex.Message}",
+                    "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Environment.Exit(1);
+            }
+        }
 }
