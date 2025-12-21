c9f937c90c555a4dd6236a85906f166590dea377
Каракулло Лев <CrossoutGamer226@gmail.com>
2025-12-21 23:50:02 +0600

    Добавил проверку на наличие необходимых файлов и папок для работы игры

--- TextQuestGame/Presenter/ApplicationController.cs
+++ TextQuestGame/Presenter/ApplicationController.cs
@@ -4,28 +4,24 @@ using System.Text;
 
 namespace TextQuestGame.Presenter
 {
-    internal class ApplicationController
+    public class ApplicationController : IDisposable
     {
         private GamePresenter _presenter;
         private IGameService _gameService;
         private IGameView _view;
+
         public void Start()
         {
             try
             {
-           
                 CheckRequiredFiles();
 
-                
                 _gameService = new GameService();
 
-                
                 _view = new MainForm();
 
-                
                 _presenter = new GamePresenter(_view, _gameService);
 
-                
                 Application.Run(_view as Form);
             }
             catch (Exception ex)
@@ -35,4 +31,20 @@ namespace TextQuestGame.Presenter
                 Environment.Exit(1);
             }
         }
-}
+
+        private void CheckRequiredFiles()
+        {
+            if (!File.Exists("scenes.json"))
+            {
+                CreateDefaultScenesFile();
+            }
+
+            if (!Directory.Exists("Images"))
+            {
+                Directory.CreateDirectory("Images");
+                MessageBox.Show("Создана папка Images для картинок. Добавьте туда файлы .jpg для сцен.",
+                    "Информация", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+       
