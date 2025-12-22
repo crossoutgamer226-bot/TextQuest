6526a8f64bb9211ddaf59a512a1e55338fc698e8
gomselars@gmail.com <gomselars@gmail.com>
2025-12-22 10:20:01 +0700

    Удален интрфейс GameService, заменен на новые интерфейсы взаимодествия и фасад

--- TextQuestGame/Interfaces.cs
+++ TextQuestGame/Interfaces.cs
@@ -17,12 +17,52 @@ namespace TextQuestGame
         List<string> Inventory { get; }
     }
 
-    public interface IGameService
+    public interface IGameStateService
     {
-        void SaveGame(string path);
-        void LoadGame(string path);
+        string CurrentSceneId { get; set; }
+        List<string> Inventory { get; }
+        Dictionary<string, object> Variables { get; }
+
+        void AddItem(string item);
+        void RemoveItem(string item);
+        bool HasItem(string item);
+        void SetVariable(string name, object value);
+        T GetVariable<T>(string name, T defaultValue = default);
+    }
+
+    public interface ISceneService
+    {
+        IScene GetScene(string sceneId);
+        IScene GetCurrentScene(IGameStateService state);
+        string GetSceneImagePath(string sceneId);
+        List<Choice> GetAvailableChoices(IScene scene, IGameStateService state);
+    }
+
+    public interface IChoiceService
+    {
+        bool CheckCondition(string condition, IGameStateService state);
+        void ApplyEffect(string effect, IGameStateService state);
+        void ProcessChoice(Choice choice, IGameStateService state);
+    }
+
+    public interface ISaveLoadService
+    {
+        void SaveGame(string path, IGameStateService state);
+        void LoadGame(string path, IGameStateService state);
+        void ResetGame(IGameStateService state);
+    }
+
+    public interface IGameFacade
+    {
         IScene GetCurrentScene();
         void MakeChoice(int choiceIndex);
+        void SaveGame(string path);
+        void LoadGame(string path);
+        void ResetGame();
+        List<string> GetAvailableChoices();
+        string GetCurrentSceneImagePath();
+        List<string> GetInventory();
     }
+
 
 }
