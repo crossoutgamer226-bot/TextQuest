37a59fe23505286d97cbff5a2881b3cc78392bcf
gomselars@gmail.com <gomselars@gmail.com>
2025-12-23 20:17:49 +0700

    Добавлены тестовые данные, тест базовыз моделей

---
+++ TextQuestGame.Tests/Utils/TestData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using TextQuestGame;
+
+namespace TextQuestGame.Tests
+{
+    public static class TestData
+    {
+        public static Scene CreateScene() => new Scene
+        {
+            Id = "test",
+            Text = "Тест",
+            ImagePath = "test.jpg",
+            Choices = new List<Choice>
+            {
+                new Choice { Text = "Выбор", NextSceneId = "next" }
+            }
+        };
+
+        public static GameState CreateGameState() => new GameState
+        {
+            CurrentSceneId = "start",
+            Inventory = new List<string> { "Item1", "Item2" }
+        };
+    }
+}
